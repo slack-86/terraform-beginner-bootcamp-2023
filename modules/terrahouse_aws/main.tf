7019ed0dfeb4fdaf3e6f3497756dc243e3415eef
slack-86 <slack86@icloud.com>
2023-09-27 19:34:15 +0000

    Progress adding object to S3 for #25

--- modules/terrahouse_aws/main.tf
+++ modules/terrahouse_aws/main.tf
@@ -19,3 +19,20 @@ resource "aws_s3_bucket" "terrahome_bucket" {
     userUUID = var.user_uuid
   }
 }
+resource "aws_s3_bucket_website_configuration" "terrahome_configuration" {
+  bucket = aws_s3_bucket.terrahome_bucket.bucket
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+resource "aws_s3_object" "terrahome_index" {
+  bucket = aws_s3_bucket.terrahome_bucket.bucket
+  key = "index.html"
+  source = "${path.root}/public/index.html"
+  etag = filemd5("${path.root}/public/index.html")  
+}
--- modules/terrahouse_aws/outputs.tf
+++ modules/terrahouse_aws/outputs.tf
@@ -1,3 +1,6 @@
 output "bucket_name" {
     value = aws_s3_bucket.terrahome_bucket.bucket
 }
+output "terrahome_endpoint" {
+    value = aws_s3_bucket_website_configuration.terrahome_configuration.website_endpoint
+}
