c455671ee442664d4b077a8eb172d17e8c7b6bb2
slack-86 <slack86@icloud.com>
2023-10-05 17:59:32 +0000

    wip setting up custom provider #39

--- terraform-provider-terratowns/main.go
+++ terraform-provider-terratowns/main.go
@@ -1,23 +1,23 @@
 package main
 
-import {
+import (
 	"fmt"
-	"log"
+	// "log"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-
-}
+	// "github.com/google/uuid"
+)
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: Provider
+		ProviderFunc: Provider,
 	})
 	// Format.PrintLine
 	fmt.Println("Hello, world!")
 }
 
 // in Go a titlecase function will get exported
-func Prover() *schema.Provier {
+func Provider() *schema.Provider {
 	var p *schema.Provider
 	p = &schema.Provider{
 		ResourcesMap: map[string]*schema.Resource{
@@ -26,27 +26,27 @@ func Prover() *schema.Provier {
 		DataSourcesMap: map[string]*schema.Resource{
 
 		},
-		Schema: map[string]*schema.Resource{
+		Schema: map[string]*schema.Schema{
 			"endpoint": {
 				Type: schema.TypeString,
 				Required: true,
-				Description: "The endpoint for the external endpoint"
-			}
+				Description: "The endpoint for the external endpoint",
+			},
 			"token": {
 				Type: schema.TypeString,
 				Required: true,
 				Sensitive: true, // make the token hidden in logs
-				Description: "Bearer token for authorization"
-			}
+				Description: "Bearer token for authorization",
+			},
 			"user_uuid": {
 				Type: schema.TypeString,
 				Required: true,
-				Description: "UUID for configuration"
+				Description: "UUID for configuration",
 				// ValidateFunc: validateUUID,
-			}
-		}
+			},
+		},
 	}
-	p.ConfigureContextFunc = providerConfigure(p)
+	// p.ConfigureContextFunc = providerConfigure(p)
 	return p
 }
 
